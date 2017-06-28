021ccec164723f8a269aa73e883cd08d6d9bd163
Macky Sagales <mackysagales@gmail.com>
2017-06-28 21:43:10 +0800

    add dev and rev and checkbox

--- angular/other_gqm_validation.js
+++ angular/other_gqm_validation.js
@@ -1,16 +1,81 @@
 var myApp = angular.module('app', []);
   myApp.controller('CodeCtrl', function($scope) {
 
-  $scope.choices = [{id: 'choice1'}];
+  $scope.dev_list = [{id: 'dev1'}];
+  $scope.rev_list = [{id: 'rev1'}];
   
-  $scope.addNewChoice = function() {
-    var newItemNo = $scope.choices.length+1;
-    $scope.choices.push({'id':'choice'+newItemNo});
+  $scope.addNewDev = function() {
+    var newItemNo = $scope.dev_list.length+1;
+    $scope.dev_list.push({'id':'dev'+newItemNo});
   };
     
-  $scope.removeChoice = function() {
-    var lastItem = $scope.choices.length-1;
-    $scope.choices.splice(lastItem);
+  $scope.removeDev = function() {
+    var lastItem = $scope.dev_list.length-1;
+    $scope.dev_list.splice(lastItem);
+  };
+  
+  $scope.addNewRev = function() {
+    var newItemNo = $scope.rev_list.length+1;
+    $scope.rev_list.push({'id':'rev'+newItemNo});
+  };
+    
+  $scope.removeRev = function() {
+    var lastItem = $scope.rev_list.length-1;
+    $scope.rev_list.splice(lastItem);
+  };
+  
+});
+
+  myApp.controller('UTPCtrl', function($scope) {
+
+  $scope.dev_list = [{id: 'dev1'}];
+  $scope.rev_list = [{id: 'rev1'}];
+  
+  $scope.addNewDev = function() {
+    var newItemNo = $scope.dev_list.length+1;
+    $scope.dev_list.push({'id':'dev'+newItemNo});
+  };
+    
+  $scope.removeDev = function() {
+    var lastItem = $scope.dev_list.length-1;
+    $scope.dev_list.splice(lastItem);
+  };
+  
+  $scope.addNewRev = function() {
+    var newItemNo = $scope.rev_list.length+1;
+    $scope.rev_list.push({'id':'rev'+newItemNo});
+  };
+    
+  $scope.removeRev = function() {
+    var lastItem = $scope.rev_list.length-1;
+    $scope.rev_list.splice(lastItem);
+  };
+  
+});
+
+  myApp.controller('UTECtrl', function($scope) {
+
+  $scope.dev_list = [{id: 'dev1'}];
+  $scope.rev_list = [{id: 'rev1'}];
+  
+  $scope.addNewDev = function() {
+    var newItemNo = $scope.dev_list.length+1;
+    $scope.dev_list.push({'id':'dev'+newItemNo});
+  };
+    
+  $scope.removeDev = function() {
+    var lastItem = $scope.dev_list.length-1;
+    $scope.dev_list.splice(lastItem);
+  };
+  
+  $scope.addNewRev = function() {
+    var newItemNo = $scope.rev_list.length+1;
+    $scope.rev_list.push({'id':'rev'+newItemNo});
+  };
+    
+  $scope.removeRev = function() {
+    var lastItem = $scope.rev_list.length-1;
+    $scope.rev_list.splice(lastItem);
   };
   
 });
--- application/views/modals/add_project_requirement.php
+++ application/views/modals/add_project_requirement.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
     
       <!-- Modal content-->
-      <div class="modal-content" >
+      <div class="modal-content">
 		
 			<div class="modal-header" style="background-color:#333;color:#fff;">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -37,12 +37,13 @@
 			<!--CODE-->
 			<div class="check-element animate-show-hide" ng-show="code" ng-controller="CodeCtrl">
 			<hr />
-			
+			<label> CODE </label>
 			<div class="row">
+				
 				<div class="col-md-12">
 					<label>STATUS</label>
 					<?php echo form_dropdown('status', $this->db->enum_select('project_req','status'),'','class="form-control"'); ?>
-					<input type="text" ng-model="choice.dev" name="assignee_id[]" placeholder="Developer" required hidden>
+					<input type="text" ng-model="item.dev" name="" placeholder="Developer" required hidden>
 				</div>
 			</div>
 				
@@ -50,66 +51,209 @@
 			
 			<div class="row">
 				<div class="col-md-6">
-				<fieldset  data-ng-repeat="choice in choices">
-					<div>
+				<div>
 						<label>ASSIGNEE</label>
+				</div>
+				<fieldset  data-ng-repeat="dev_item in dev_list">
+					<div class="input-group">
+					
+						<select class="form-control col-xs-5" name="assignee_id[]" ng-model="dev_item.dev" >
+							<?php foreach($eid as $assignee){ 
+							echo '<option value="'.$assignee->id.'">'.$assignee->eid.'</option>';
+						}?> </select>
+					
+						<span class="input-group-btn">
+							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeDev()"><span
+							class="glyphicon glyphicon-remove"></span></button>
+						</span>
 					</div>
+					<br />
+				</fieldset>
+				
+				<br />
+				<button class="btn btn-success addfields" ng-click="addNewDev()">Add Assignee</button>
+				
+				</div>
+				
+				<div>
+					<label>REVIEWER</label>
+				</div>
+				<div class="col-md-6">
+				<fieldset  data-ng-repeat="rev_item in rev_list">
+					
 					
 					<div class="input-group">
-						<select class="form-control" name="assignee_id[]" ng-model="choice.dev">
+						<select class="form-control" name="assignee_id[]" ng-model="rev_item.rev">
 							<?php foreach($eid as $assignee){ 
 							echo '<option value="'.$assignee->id.'">'.$assignee->eid.'</option>';
 						}?> </select>
 						
 						<span class="input-group-btn">
-							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeChoice()"><span
-							class="glyphicon glyphicon-remove"></span></button>
-						</span>
-					</div>	
-				</fieldset>
-				
-				<br />
-				<button class="btn btn-success addfields" ng-click="addNewChoice()">Add Assignee</button>
-				
-				</div>
-				
-				
-				
-				<div class="col-md-6">
-				<fieldset  data-ng-repeat="choice in choices">
-					<div>
-						<label>REVIEWER</label>
-					</div>
-					
-					
-					<div class="input-group">
-						<select class="form-control " name="reviewer_id[]" ng-model="choice.rev">
-						<?php foreach($eid as $reviewer){ 
-						echo '<option value="'.$reviewer->id.'">'.$reviewer->eid.'</option>';
-						}?> </select>
-						
-						<span class="input-group-btn">
-							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeChoice()"><span
+							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeRev()"><span
 							class="glyphicon glyphicon-remove"></span></button>
 						</span>
 					</div>
-					
+					<br />					
 				</fieldset>
 				
 				<br />
-				<button class="btn btn-success addfields" ng-click="addNewChoice()">Add Reviewer</button>
-				</div>
+				<button class="btn btn-success addfields" ng-click="addNewRev()">Add Reviewer</button>
 				
+				</div>
+					
 			</div>
 			
 			<br />
 			
-			 <div id="choicesDisplay">
-				{{ choices }}
-			 </div>
-			 <hr />
+			</div>
+			<!--END OF CODE-->
+			<!--UTP-->
+			<div class="check-element animate-show-hide" ng-show="utp" ng-controller="UTPCtrl">
+			<hr />
+			<label> UTP </label>
+			<div class="row">
+				
+				<div class="col-md-12">
+					<label>STATUS</label>
+					<?php echo form_dropdown('status', $this->db->enum_select('project_req','status'),'','class="form-control"'); ?>
+					<input type="text" ng-model="item.dev" name="" placeholder="Developer" required hidden>
+				</div>
+			</div>
+				
+			<br />
+			
+			<div class="row">
+				<div class="col-md-6">
+				<div>
+						<label>ASSIGNEE</label>
+				</div>
+				<fieldset  data-ng-repeat="dev_item in dev_list">
+					<div class="input-group">
+					
+						<select class="form-control col-xs-5" name="assignee_id[]" ng-model="dev_item.dev" >
+							<?php foreach($eid as $assignee){ 
+							echo '<option value="'.$assignee->id.'">'.$assignee->eid.'</option>';
+						}?> </select>
+					
+						<span class="input-group-btn">
+							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeDev()"><span
+							class="glyphicon glyphicon-remove"></span></button>
+						</span>
+					</div>
+					<br />
+				</fieldset>
+				
+				<br />
+				<button class="btn btn-success addfields" ng-click="addNewDev()">Add Assignee</button>
+				
+				</div>
+				
+				<div>
+					<label>REVIEWER</label>
+				</div>
+				<div class="col-md-6">
+				<fieldset  data-ng-repeat="rev_item in rev_list">
+					
+					
+					<div class="input-group">
+						<select class="form-control" name="assignee_id[]" ng-model="rev_item.rev">
+							<?php foreach($eid as $assignee){ 
+							echo '<option value="'.$assignee->id.'">'.$assignee->eid.'</option>';
+						}?> </select>
+						
+						<span class="input-group-btn">
+							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeRev()"><span
+							class="glyphicon glyphicon-remove"></span></button>
+						</span>
+					</div>
+					<br />					
+				</fieldset>
+				
+				<br />
+				<button class="btn btn-success addfields" ng-click="addNewRev()">Add Reviewer</button>
+				
+				</div>
+					
 			</div>
 			
+			<br />
+			
+			</div>
+			<!--END OF UTE-->
+			
+			<!--UTE-->
+			<div class="check-element animate-show-hide" ng-show="ute" ng-controller="UTECtrl">
+			<hr />
+			<label> UTE </label>
+			<div class="row">
+				
+				<div class="col-md-12">
+					<label>STATUS</label>
+					<?php echo form_dropdown('status', $this->db->enum_select('project_req','status'),'','class="form-control"'); ?>
+					<input type="text" ng-model="item.dev" name="" placeholder="Developer" required hidden>
+				</div>
+			</div>
+				
+			<br />
+			
+			<div class="row">
+				<div class="col-md-6">
+				<div>
+						<label>ASSIGNEE</label>
+				</div>
+				<fieldset  data-ng-repeat="dev_item in dev_list">
+					<div class="input-group">
+					
+						<select class="form-control col-xs-5" name="assignee_id[]" ng-model="dev_item.dev" >
+							<?php foreach($eid as $assignee){ 
+							echo '<option value="'.$assignee->id.'">'.$assignee->eid.'</option>';
+						}?> </select>
+					
+						<span class="input-group-btn">
+							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeDev()"><span
+							class="glyphicon glyphicon-remove"></span></button>
+						</span>
+					</div>
+					<br />
+				</fieldset>
+				
+				<br />
+				<button class="btn btn-success addfields" ng-click="addNewDev()">Add Assignee</button>
+				
+				</div>
+				
+				<div>
+					<label>REVIEWER</label>
+				</div>
+				<div class="col-md-6">
+				<fieldset  data-ng-repeat="rev_item in rev_list">
+					
+					
+					<div class="input-group">
+						<select class="form-control" name="assignee_id[]" ng-model="rev_item.rev">
+							<?php foreach($eid as $assignee){ 
+							echo '<option value="'.$assignee->id.'">'.$assignee->eid.'</option>';
+						}?> </select>
+						
+						<span class="input-group-btn">
+							<button class="btn btn-danger remove" ng-show="$last" ng-click="removeRev()"><span
+							class="glyphicon glyphicon-remove"></span></button>
+						</span>
+					</div>
+					<br />					
+				</fieldset>
+				
+				<br />
+				<button class="btn btn-success addfields" ng-click="addNewRev()">Add Reviewer</button>
+				
+				</div>
+					
+			</div>
+			
+			<br />
+			
+			</div>
+			<!--END OF UTE-->
 			
 			
         </div>
